2e9d3f187de25755827f9a0b96c3b5f5d0828592
arctair <jtyler_@outlook.com>
2017-03-28 17:32:44 -0500

    added config.json

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var config = require('./config.json');
 
 app.use('/public',express.static(__dirname+'/public'));
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/public/index.html');
 });
-http.listen(3000, function(){
-	console.log("listening on *:3000");
+http.listen(config.port, function(){
+	console.log('listening on *:' + config.port);
 });
